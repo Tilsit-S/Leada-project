dc107f475cb946bfd6142a8d7cff6332bae86a7d
b0rntokill <zappbranigan39@gmail.com>
2023-06-16 00:39:09 +0200

    Fix body no-scroll

--- source/js/index.js
+++ source/js/index.js
@@ -226,6 +226,7 @@
     const closeBurgerBtn = document.querySelector(".burger-btn-close");
     const menu = document.querySelector(".pop-up");
     const overlay = document.querySelector(".modal-overlay");
+    const body = document.querySelector("body");
 
     function onOverlayClick(evt) {
         if (evt.target.classList.contains("modal-overlay")) {
@@ -237,12 +238,14 @@
         menu.classList.remove("show");
         overlay.classList.remove("open");
         overlay.removeEventListener("click", onOverlayClick);
+        body.classList.remove("no-scroll");
     }
 
     function onBurgerButtonClick(evt) {
         evt.preventDefault();
         menu.classList.add("show");
         overlay.classList.add("open");
+        body.classList.add("no-scroll");
         overlay.addEventListener("click", onOverlayClick);
     }
 
