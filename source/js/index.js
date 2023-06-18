bdd0afdbe5503b04bdbe8270d6a8dbc7dd954d59
b0rntokill <zappbranigan39@gmail.com>
2023-06-18 14:07:17 +0200

    Adds light zoom

--- source/js/index.js
+++ source/js/index.js
@@ -252,3 +252,44 @@
     burgerButton.addEventListener("click", onBurgerButtonClick);
     closeBurgerBtn.addEventListener("click", closeMenu);
 })();
+
+// слайдер
+$(document).ready(function () {
+    $(".price__list").slick({
+        infinite: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1270,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 680,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+});
+
+// увеличение картинок
+$(function () {
+    $(".lightzoom").lightzoom({
+        overlayOpacity: "0.7",
+        isOverlayClickClosing: true,
+        isEscClosing: true,
+        overlayColor: "#000000",
+    });
+});
